872562f50b7bc2d18ef9745dc251f7e04b4a483c
Will Boyd <will@codersblock.com>
2018-11-03 18:03:29 -0400

    Language translation and writing output file

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const fs = require('fs');
 const minimist = require('minimist');
-const path = require('path');
 const request = require('request');
 
-let argv, gists = [];
+const languageMap = {
+    'batchfile': 'batch',
+    'c++': 'cpp',
+    'c#': 'csharp',
+    'f#': 'fsharp',
+    'html': 'markup',
+    'shell': 'bash',
+    'xml': 'markup'
+};
+
+let argv, fileContent, gists = [];
 
 function init() {
 	argv = minimist(process.argv.slice(2), {
@@ -17,25 +26,24 @@ function init() {
     if (!argv.input) {
         console.log('Input file not specified.');
     } else {
-        //let outputFilename = argv.input.replace('.', '-output.');
-        let content = readInputFile();
-        processContent(content);
+        readInputFile();
+        processFileContent();
     }
 }
 
 function readInputFile() {
 	try {
-		return fs.readFileSync(argv.input, 'utf8');
+		fileContent = fs.readFileSync(argv.input, 'utf8');
 	} catch (ex) {
 		console.log('Unable to read input file.');
 		console.log(ex.message);
 	}
 }
 
-function processContent(content) {
+function processFileContent() {
     let regex = (/<script src="https:\/\/gist\.github\.com\/[^\/]+\/(\d+)\.js"><\/script>/gi);
     let match;
-    while ((match = regex.exec(content)) !== null) {
+    while ((match = regex.exec(fileContent)) !== null) {
         gists.push({
             isProcessed: false,
             scriptTag: match[0],
@@ -56,8 +64,8 @@ function processContent(content) {
         };
         if (argv.username && argv.token) {
             options.auth = {
-                'user': argv.username,
-                'pass': argv.token
+                user: argv.username,
+                pass: argv.token
             };
         }
         request.get(options, gistGetCallback.bind(this, gist));
@@ -85,7 +93,32 @@ function gistGetCallback(gist, error, response, body) {
 }
 
 function writeOutputFile() {
-    console.log('Finish up!');
+    gists.forEach(gist => {
+        let prismLang = translateLanguage(gist.language);
+        let classAttr = prismLang ? ' class="language-' + prismLang + '"' : '';
+        let codeBlock = '<pre><code' + classAttr + '>' + gist.content + '</code></pre>';
+        fileContent = fileContent.replace(gist.scriptTag, codeBlock);
+    });
+
+    let outputFile = argv.input.replace('.', '-output.');
+    fs.writeFile(outputFile, fileContent, (err) => {
+        if (err) {
+            console.log('Unable to write output file.');
+            console.log(err);
+        } else {
+            console.log('Wrote ' + outputFile + '.');
+        }
+    });
+}
+
+function translateLanguage(gistLang) {
+    gistLang = (gistLang || '').toLowerCase();
+    if (!gistLang || gistLang === 'text') {
+        return null;
+    } else {
+        let prismLang = languageMap[gistLang] || gistLang;
+        return prismLang;
+    }
 }
 
 // it's go time!
